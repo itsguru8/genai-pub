7d1d54bc49be46afb7736930e2550edc6b034a2b
itsguru8 <itsguru.8@gmail.com>
2024-09-11 12:48:51 +0000

    Add files via upload

--- prepare-input.py
+++ prepare-input.py
@@ -6,10 +6,12 @@ import os
 import re
 import json
 
-file_path = "good-TR-JSON.txt"
-inputfile_for_localAI = "input_for_localAI.txt"
+#file_path = "good-TR-JSON.txt"
+file_path = "pilotTRsDump.json"
+inputfile_for_localAI = "input_for_localAI.json"
 ericsson_terms_file = 'ericsson-terms-remove.txt'
 final_json_out = []
+final_json_key_val = {}
 
 
 if file_path:
@@ -24,48 +26,80 @@ if file_path:
                 # what keys & corresponding field names are H/C below
 
                 """    
-                "AI_input_fields": [
-                "key": "key",
+                "Issue-id": "key",
                 "summary":"['fields']['summary']",
                 "Issue Description" : "['fields']['customfield_25119']",
                 "Impact": "['fields']['customfield_30068']",
                 "Resolution Details" : "['fields']['customfield_39227']"
+                Test phase (.val): 19973
+                SHBF(P) (.val): 48911
+                SHBF(T)(.val): 48912
+                Reason for slippage (.val): 48913
                 ] """
+
+                if i['key']:
+                  print("processing : ",i['key'],"..")
 
                 ### REMOVE E// data before write !! 
                 ###
-                with open(ericsson_terms_file, 'r') as erc_file:
+                  with open(ericsson_terms_file, 'r') as erc_file:
                     line = erc_file.readline()
                     while line:
                         #print("e// term = "+line)
                         # the re.IGNORECASE is used to ignore cases
                         compileObj = re.compile(re.escape(line.strip()), re.IGNORECASE)
                         #Substitute the substring with replacing a string using the regex sub() function
-
+    
                         #Replace in all fields
                         i['key'] = compileObj.sub('', i['key'])
-                        i['fields']['summary'] = compileObj.sub('', i['fields']['summary'])
-                        i['fields']['customfield_25119'] = compileObj.sub('', i['fields']['customfield_25119'])
-                        i['fields']['customfield_30068'] = compileObj.sub('', i['fields']['customfield_30068'])
+                        if i['fields']['summary'] :
+                            i['fields']['summary'] = compileObj.sub('', i['fields']['summary'])
+                        if i['fields']['customfield_25119'] :
+                            i['fields']['customfield_25119'] = compileObj.sub('', i['fields']['customfield_25119'])
+                        if i['fields']['customfield_30068'] :
+                            i['fields']['customfield_30068'] = compileObj.sub('', i['fields']['customfield_30068'])
                         if i['fields']['customfield_39227']: 
                             i['fields']['customfield_39227'] = compileObj.sub('', i['fields']['customfield_39227'])
-                        #i['key'] = i['key'].replace(line.strip(),'')
-                        line = erc_file.readline()
+                            #Test phase
+                        if i['fields']['customfield_19973'] :
+                          if i['fields']['customfield_19973']['value'] :
+                            i['fields']['customfield_19973']['value'] = compileObj.sub('', i['fields']['customfield_19973']['value'])
+                            #shbf-p
+                        if i['fields']['customfield_48911'] :
+                          if i['fields']['customfield_48911']['value'] :
+                            i['fields']['customfield_48911']['value'] = compileObj.sub('', i['fields']['customfield_48911']['value'])
+                            #shbf-t
+                        if i['fields']['customfield_48912'] :
+                          if i['fields']['customfield_48912']['value'] :
+                            i['fields']['customfield_48912']['value'] = compileObj.sub('', i['fields']['customfield_48912']['value'])
+                            #RFS
+                        if i['fields']['customfield_48913'] :
+                          if i['fields']['customfield_48913']['value'] :
+                            i['fields']['customfield_48913']['value'] = compileObj.sub('', i['fields']['customfield_48913']['value'])
+
+                    line = erc_file.readline()
                 #print("updated key= "+i['key'])
                 ###
-
+                # target object - outjson
                 outjson = {}
-                outjson['key'] = i['key']
-                outjson['summary'] = i['fields']['summary']
-                outjson['Issue Description'] = i['fields']['customfield_25119']
-                outjson['Impact'] = i['fields']['customfield_30068']
-                outjson['Resolution Details'] = i['fields']['customfield_39227']
-                #check if "fields" are read fine 
-                print("wrote key ="+outjson['key'])
-
+                content = {}
+                outjson['Issue-Id'] = i['key']
+                content['summary'] = i['fields']['summary']
+                content['Issue Description'] = i['fields']['customfield_25119']
+                content['Impact'] = i['fields']['customfield_30068']
+                content['Resolution Details'] = i['fields']['customfield_39227']
+                content['Test Phase(found)'] = i['fields']['customfield_19973']['value']
+                content['Should have been found in (phase)'] = i['fields']['customfield_48911']['value']
+                content['Should have been found in (type)'] = i['fields']['customfield_48912']['value']
+                content['Reason for slippage'] = i['fields']['customfield_48913']['value']
+                outjson['fields'] = content
 
                 final_json_out.append(outjson)
+                print("wrote key ="+outjson['Issue-Id'])
 
-            json.dump(final_json_out, outfile)
+            final_json_key_val['Issues'] = final_json_out
+            json.dump(final_json_key_val, outfile)
+
+            print ("see file :"+inputfile_for_localAI)
 
 
